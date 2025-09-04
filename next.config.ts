bfd85eb2295d28bcf0b1ae7b1efd25b595596c10
1903812532 <1903812532@qq.com>
2025-09-04 19:33:34 +0800

    first

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+const nextConfig = {
+output: "export",
+basePath: "/yunda",
+assetPrefix: "/yunda/",
 };
+
 
 export default nextConfig;
